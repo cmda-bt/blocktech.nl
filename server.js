05e1455902e17c19b84768f80e4f4dc97ee4759d
dandevri <hi@dandevri.es>
2020-05-16 20:04:27 +0200

    Add pug templating

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
+
 const app = express()
 const port = 3000
 
 app.use('/static', express.static('static'));
+app.set('view engine', 'pug')
 
 app.get('/', home);
 
 function home(req, res) {
-  res.send('Hello World');
+  res.render('index');
 }
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
