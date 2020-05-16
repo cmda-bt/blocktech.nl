0d7416e408394d0898a0fc087a52e9ea2f450787
dandevri <hi@dandevri.es>
2020-05-16 20:25:35 +0200

    Add routes

--- server.js
+++ server.js
@@ -6,10 +6,25 @@ const port = 3000
 app.use('/static', express.static('static'));
 app.set('view engine', 'pug')
 
-app.get('/', home);
+app.get('/', index);
+app.get('/back-end', backend);
+app.get('/front-end', frontend);
+app.get('/project-tech', project);
 
-function home(req, res) {
+function index(req, res) {
   res.render('index');
+}
+
+function backend(req, res) {
+  res.render('back-end');
+}
+
+function frontend(req, res) {
+  res.render('front-end');
+}
+
+function project(req, res) {
+  res.render('project-tech');
 }
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
