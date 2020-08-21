0129afafdd5565fabc280abe8aa9be7e2edc0ed0
dandevri <hi@dandevri.es>
2020-08-21 11:50:16 +0200

    Add classlist functionality

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+const students = require('./data/students_20-21.json');
+
 app.use('/static', express.static('static'));
 app.set('view engine', 'pug')
 
@@ -10,6 +12,7 @@ app.get('/', index);
 app.get('/back-end', backend);
 app.get('/front-end', frontend);
 app.get('/project-tech', project);
+app.get('/classlist', classlist);
 
 function index(req, res) {
   res.render('index');
@@ -25,6 +28,10 @@ function frontend(req, res) {
 
 function project(req, res) {
   res.render('project-tech');
+}
+
+function classlist(req, res) {
+  res.render('classlist', { students });
 }
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
